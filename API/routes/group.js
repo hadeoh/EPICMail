fb1b1bc8edd53ec9548d59d5c5b3eea68f2178cf
Usman Adio <usthmandanfodio@gmail.com>
2019-04-19 03:25:04 -0700

    Complete unit tests for endpoints

--- API/routes/group.js
+++ API/routes/group.js
@@ -15,7 +15,7 @@ router.post('/:id/users', Auth.verifyToken, GroupController.addUserToGroup);
 
 router.patch('/:id/name', Auth.verifyToken, GroupController.editGroupName);
 
-router.delete('/:id', Auth.verifyToken, GroupController.deleteGroup);
+router.delete('/:id', Auth.verifyToken, GroupController.deleteAGroup);
 
 router.delete('/:id/users/:userId', Auth.verifyToken, GroupController.deleteUserFromGroup);
 
--- API/tests/group.test.js
+++ API/tests/group.test.js
@@ -10,9 +10,9 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 const token = jwt.sign(
-  { userEmail: 'vincicode@epicmail.com' },
+  { userId: 1 },
   process.env.SECRET,
-  { expiresIn: '7d' },
+  { expiresIn: '365d' },
 );
 
 describe('Groups', () => {
@@ -21,7 +21,7 @@ describe('Groups', () => {
     it('should create a group', (done) => {
       chai.request(app)
         .post('/api/v1/groups')
-        .set('x-access-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTU1NTAwMTI5MiwiZXhwIjoxNTg2NTM3MjkyfQ.KvtaExEg-L6MIr5OTq8n-JllW-Nc7nYlJbDX3PhoJ1E')
+        .set('x-access-token', token)
         .send({
           name: 'azeez',
         })
@@ -38,22 +38,14 @@ describe('Groups', () => {
     });
     // Test to add user to group
     it('should add user to a group', (done) => {
-      const adminToken = jwt.sign(
-        { userEmail: 'franchesqa@epicmail.com' },
-        process.env.SECRET,
-        { expiresIn: '7d' },
-      );
       chai.request(app)
-        .post('api/v1/groups/1/users')
-        .set('x-access-token', adminToken)
+        .post('/api/v1/groups/1/users')
+        .set('x-access-token', token)
         .send({
-          groupId: 1,
-          userId: 3,
-          userEmail: 'toluniyin@epicmail.com',
+          userEmail: 'vincicode@epicmail.com',
         })
         .end((err, res) => {
           if (err) done(err);
-          console.log(res.body);
           expect(res.status).to.equal(201);
           expect(res.body).to.be.an('object');
           expect(res.body).to.have.keys('status', 'data');
@@ -63,16 +55,19 @@ describe('Groups', () => {
           done();
         });
     });
-    // Test to send email to group
-    it('should send email to a group', (done) => {
+    // Test to create group messages
+    it('should create a group message', (done) => {
+      const memToken = jwt.sign(
+        { userId: 4 },
+        process.env.SECRET,
+        { expiresIn: '365d' },
+      );
       chai.request(app)
-        .post('api/v1/groups/1/messages')
-        .set('x-access-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjYsImlhdCI6MTU1NTU4Njg5NSwiZXhwIjoxNTg3MTIyODk1fQ.0UUL5qQMvQPJXClDwmf7SFb3uuAIfy7iZdcbDEDvlTo')
+        .post('/api/v1/groups/1/messages')
+        .set('x-access-token', memToken)
         .send({
-          groupId: 1,
-          senderEmail: 'adiousman@epicmail.com',
-          subject: 'dancing',
-          message: 'dancing is sweet',
+          subject: 'azeez',
+          message: 'azeez is a boy',
         })
         .end((err, res) => {
           if (err) done(err);
@@ -82,6 +77,240 @@ describe('Groups', () => {
           expect(res.body.data).to.be.an('array');
           expect(res.body.status).to.be.a('number');
           expect(res.body.status).to.equal(201);
+          done();
+        });
+    });
+    // Test to update group name
+    it('should edit a group name', (done) => {
+      chai.request(app)
+        .patch('/api/v1/groups/1/name')
+        .set('x-access-token', token)
+        .send({
+          name: 'usman',
+        })
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.keys('status', 'data');
+          expect(res.body.data).to.be.an('array');
+          expect(res.body.status).to.be.a('number');
+          expect(res.body.status).to.equal(200);
+          done();
+        });
+    });
+    // Test to delete a group
+    it('should delete a group', (done) => {
+      const memToken = jwt.sign(
+        { userId: 4 },
+        process.env.SECRET,
+        { expiresIn: '365d' },
+      );
+      chai.request(app)
+        .delete('/api/v1/groups/6')
+        .set('x-access-token', memToken)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.keys('status', 'data');
+          expect(res.body.data).to.be.an('array');
+          expect(res.body.status).to.be.a('number');
+          expect(res.body.status).to.equal(200);
+          done();
+        });
+    });
+    // Test to delete user from group
+    it('should delete user from a group', (done) => {
+      const memToken = jwt.sign(
+        { userId: 4 },
+        process.env.SECRET,
+        { expiresIn: '365d' },
+      );
+      chai.request(app)
+        .delete('/api/v1/groups/1/users/4')
+        .set('x-access-token', memToken)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.keys('status', 'data');
+          expect(res.body.data).to.be.an('array');
+          expect(res.body.status).to.be.a('number');
+          expect(res.body.status).to.equal(200);
+          done();
+        });
+    });
+    // Test to delete message from a group
+    it('should delete message from a group', (done) => {
+      chai.request(app)
+        .delete('/api/v1/groups/1/messages/6')
+        .set('x-access-token', token)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.keys('status', 'message');
+          expect(res.body.message).to.be.an('string');
+          expect(res.body.status).to.be.a('number');
+          expect(res.body.status).to.equal(200);
+          done();
+        });
+    });
+    it('should get all groups', (done) => {
+      const memToken = jwt.sign(
+        { userId: 4 },
+        process.env.SECRET,
+        { expiresIn: '365d' },
+      );
+      chai.request(app)
+        .get('/api/v1/groups')
+        .set('x-access-token', memToken)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.keys('status', 'data');
+          expect(res.body.data).to.be.an('array');
+          expect(res.body.status).to.be.a('number');
+          expect(res.body.status).to.equal(200);
+          done();
+        });
+    });
+    // Test to get all group users
+    it('should get all group users', (done) => {
+      chai.request(app)
+        .get('/api/v1/groups/1/users')
+        .set('x-access-token', token)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.keys('status', 'data');
+          expect(res.body.data).to.be.an('array');
+          expect(res.body.status).to.be.a('number');
+          expect(res.body.status).to.equal(200);
+          done();
+        });
+    });
+    // Test to get a group user
+    it('should get a group user', (done) => {
+      chai.request(app)
+        .get('/api/v1/groups/1/users/1')
+        .set('x-access-token', token)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.keys('status', 'data');
+          expect(res.body.data).to.be.an('array');
+          expect(res.body.status).to.be.a('number');
+          expect(res.body.status).to.equal(200);
+          done();
+        });
+    });
+    // Test to get all received group emails
+    it('should get all received group emails', (done) => {
+      const memToken = jwt.sign(
+        { userId: 2 },
+        process.env.SECRET,
+        { expiresIn: '365d' },
+      );
+      chai.request(app)
+        .get('/api/v1/groups/1/messages')
+        .set('x-access-token', memToken)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.keys('status', 'data');
+          expect(res.body.data).to.be.an('array');
+          expect(res.body.status).to.be.a('number');
+          expect(res.body.status).to.equal(200);
+          done();
+        });
+    });
+    // Test to get emails sent to a group
+    it('should get all sent group emails', (done) => {
+      const memToken = jwt.sign(
+        { userId: 2 },
+        process.env.SECRET,
+        { expiresIn: '365d' },
+      );
+      chai.request(app)
+        .get('/api/v1/groups/1/messages/sent')
+        .set('x-access-token', memToken)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.keys('status', 'data');
+          expect(res.body.data).to.be.an('array');
+          expect(res.body.status).to.be.a('number');
+          expect(res.body.status).to.equal(200);
+          done();
+        });
+    });
+    // Test to get unread group emails
+    it('should get all unread group emails', (done) => {
+      const memToken = jwt.sign(
+        { userId: 2 },
+        process.env.SECRET,
+        { expiresIn: '365d' },
+      );
+      chai.request(app)
+        .get('/api/v1/groups/1/messages/unread')
+        .set('x-access-token', memToken)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.keys('status', 'data');
+          expect(res.body.data).to.be.an('array');
+          expect(res.body.status).to.be.a('number');
+          expect(res.body.status).to.equal(200);
+          done();
+        });
+    });
+    // Test to know if user is in group
+    it('should get all unread group emails', (done) => {
+      const memToken = jwt.sign(
+        { userId: 7 },
+        process.env.SECRET,
+        { expiresIn: '365d' },
+      );
+      chai.request(app)
+        .get('/api/v1/groups/1/messages/unread')
+        .set('x-access-token', memToken)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res.status).to.equal(401);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.keys('status', 'message');
+          expect(res.body.message).to.be.an('string');
+          expect(res.body.status).to.be.a('number');
+          expect(res.body.status).to.equal(401);
+          done();
+        });
+    });
+    // Test to get a particular group email
+    it('should get a group email', (done) => {
+      const memToken = jwt.sign(
+        { userId: 2 },
+        process.env.SECRET,
+        { expiresIn: '365d' },
+      );
+      chai.request(app)
+        .get('/api/v1/groups/1/messages/5')
+        .set('x-access-token', memToken)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.keys('status', 'data');
+          expect(res.body.data).to.be.an('array');
+          expect(res.body.status).to.be.a('number');
+          expect(res.body.status).to.equal(200);
           done();
         });
     });
